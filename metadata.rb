9356c0a630b915ff3b12bd035e10eb250895816e
Christian Hofer <christian@besol.es>
2012-12-21 13:55:34 +0100

    Added adapted boundary cookbook: bprobe

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "Boundary"
+maintainer_email "ops@boundary.com"
+license          "Apache 2.0"
+description      "Installs/Configures bprobe"
+long_description "Installs/Configures bprobe"
+version          "0.2"
+
+# TODO smartos does not do anything as boundary does not support smartos !?
+%w{ ubuntu centos amazon debian redhat windows smartos }.each do |os|
+  supports os
+end
+
+depends "yum"
+depends "apt"
